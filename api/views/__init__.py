effe606ba9cacdd10b61e289248e28e1bf599fb0
Bang Dao <daotranbang@gmail.com>
2015-04-14 09:08:45 +0700

    add filter backend site_contains for app api

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -10,9 +10,24 @@ class AppSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('id', 'name', 'site', 'price')
 
 
+class FilterContains(object):
+    field = 'site'
+
+    def filter_queryset(self, request, queryset, view):
+        value = request.GET.get(self.field)
+        if value:
+            query_filter = {
+                "%s__contains" % self.field: value
+            }
+            queryset = queryset.filter(**query_filter)
+        return queryset
+
+
 class AppViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = models.App.objects.all()
     serializer_class = AppSerializer
+    filter_backends = (FilterContains, )
+
 
 router = routers.DefaultRouter()
 router.register(r'apps', AppViewSet)

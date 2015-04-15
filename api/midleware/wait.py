c61f808636d2c6d0fd8e800e1882d20e726bff8c
Bang Dao <daotranbang@gmail.com>
2015-04-15 11:32:47 +0700

    wait only 2 seconds for local api request

--- api/midleware/wait.py
+++ api/midleware/wait.py
@@ -5,5 +5,5 @@ class Wait5SecForLocalMiddleware(object):
     def process_response(self, request, response):
         if request.path.startswith('/api/') and request.META['REMOTE_ADDR'] == '127.0.0.1' \
                 and getattr(response, 'accepted_media_type', None) == 'application/json':
-            time.sleep(5)
+            time.sleep(2)
         return response

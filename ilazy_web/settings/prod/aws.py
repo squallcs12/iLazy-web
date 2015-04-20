4efee813d868518810cb62a3a2bc901e1b3bcd18
Bang Dao <daotranbang@gmail.com>
2015-04-20 11:21:20 +0700

    aws db config

--- ilazy_web/settings/prod/aws.py
+++ ilazy_web/settings/prod/aws.py
@@ -5,17 +5,17 @@ from ilazy_web.settings.base import *
 
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['RDS_DB_ENGINE'],
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
 
 ALLOWED_HOSTS = ['*']
 
 ADMINS = (('Bang', 'daotranbang@gmail.com'), )
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-EMAIL_HOST = os.environ.get('POSTMARK_SMTP_SERVER')
-EMAIL_HOST_PASSWORD = os.environ.get('POSTMARK_API_TOKEN')
-EMAIL_HOST_USER = os.environ.get('POSTMARK_API_KEY')
-EMAIL_PORT = 587
-
-BROKER_URL = os.environ.get('REDISTOGO_URL')

4ae655ccb64c46f2f91fa05186033f69ddd4ac3b
Bang Dao <daotranbang@gmail.com>
2015-04-20 17:02:08 +0700

    add coins field into user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
+from django.utils.translation import ugettext_lazy as _
+
 from accounts import models
 
-admin.site.register(models.User, auth_admin.UserAdmin)
+
+class UserAdmin(auth_admin.UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'coins')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'coins')
+
+
+admin.site.register(models.User, UserAdmin)
